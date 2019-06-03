2a3bfafeeb73260f3bd77586157dc22a513141e3
dziomdziorae <46715017+dziomdziorae@users.noreply.github.com>
2019-06-03 08:39:07 +0200

    dodanie logiki do kalkulatoru

--- kalkulator/kalkulator_logika.py
+++ kalkulator/kalkulator_logika.py
@@ -1,3 +1,20 @@
 def oblicz(operacje):
-    print(operacje)
-    return '10'
+    if operacje:
+        wynik = int(operacje[0])
+    else:
+        wynik = "0"
+    for _ in range(1, len(operacje), 2):
+        if operacje[_] == "+":
+            wynik = wynik + int(operacje[_+1])
+        elif operacje[_] == '-':
+            wynik = wynik - int(operacje[_+1])
+        elif operacje[_] == '*':
+            wynik = wynik * int(operacje[_ + 1])
+        elif operacje[_] == '/':
+            if int(operacje[_+1]):
+                wynik = wynik / int(operacje[_ + 1])
+            else:
+                wynik = "error"
+                break
+
+    return str(wynik)
